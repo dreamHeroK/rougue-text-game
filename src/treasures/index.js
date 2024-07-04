d239a6ba543af910985cc02f9333d092d359bf10
kangxx <kangxx@sany.com.cn>
2024-07-04 10:51:20 +0800

    feat: 更新

--- src/treasures/index.js
+++ src/treasures/index.js
@@ -6,6 +6,24 @@ const treasuresList = [
     isFirst: true,
     type: "direct",
     id: 1,
-    imgUrl:''
+    imgUrl: "",
+  },
+  {
+    name: "血腥之手",
+    desc: "生命值上限降低40,力量永久+3",
+    lucky: 0,
+    isFirst: true,
+    type: "direct",
+    id: 2,
+    imgUrl: "",
+  },
+  {
+    name: "闪避斗篷",
+    desc: "受到攻击时有20%几率闪避该次攻击",
+    lucky: 0,
+    isFirst: true,
+    type: "direct",
+    id: 3,
+    imgUrl: "",
   },
 ];
