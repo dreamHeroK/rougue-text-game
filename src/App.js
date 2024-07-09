8ba7b6cc3b0e78f627e5fcb077b6eff0e02a205d
kangxx <kangxx@sany.com.cn>
2024-07-09 11:50:29 +0800

    feat: 更新代码

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ import treasuresList from "./treasures/index.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {gameStyles.card}
+      <div className={gameStyles.cardWrap}>
         {treasuresList.map((item) => {
           return (
             <div key={item.id} className={gameStyles.card}>
-              <img src={item.imgUrl} alt={item.name} />
-              <p>{item.name}</p>
-              <p>{item.desc}</p>
+              <img
+                src={item.imgUrl}
+                alt={item.name}
+                className={gameStyles.icon}
+              />
+              <p className={gameStyles.name}>{item.name}</p>
+              <p className={gameStyles.desc}>{item.desc}</p>
             </div>
           );
         })}
-      </header>
+      </div>
     </div>
   );
 }
